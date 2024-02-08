cfd55ce9dc33a19af14459879b7c33ce2237456f
Jonas <jonascamargo999@gmail.com>
2024-02-08 07:37:31 -0300

    fix: fix upload component name

--- tr-frontend/src/app/app.component.ts
+++ tr-frontend/src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { OperationsListComponent } from './components/operations-list/operations-list.component';
+import { UploadComponent } from './components/upload/upload.component';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +11,9 @@ import { OperationsListComponent } from './components/operations-list/operations
   imports: [
     RouterOutlet,
     HeaderComponent,
-    OperationsListComponent
+    OperationsListComponent,
+    UploadComponent,
+    NgOptimizedImage
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- tr-frontend/src/app/components/upload/main.component.spec.ts
+++ tr-frontend/src/app/components/upload/main.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { MainComponent } from './main.component';
+import { UploadComponent } from './upload.component';
 
-describe('MainComponent', () => {
-  let component: MainComponent;
-  let fixture: ComponentFixture<MainComponent>;
+describe('UploadComponent', () => {
+  let component: UploadComponent;
+  let fixture: ComponentFixture<UploadComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MainComponent]
+      imports: [UploadComponent]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(MainComponent);
+    fixture = TestBed.createComponent(UploadComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
