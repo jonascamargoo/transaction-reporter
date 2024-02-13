1bf5f6b9e07f356b2361ac1a5a4ae64908a8845d
Jonas <jonascamargo999@gmail.com>
2024-02-12 21:53:09 -0300

    feat: add new home description

--- tr-frontend/src/app/components/home/home.component.ts
+++ tr-frontend/src/app/components/home/home.component.ts
@@ -1,22 +1,23 @@
-import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 import { AboutCNABComponent } from '../about-cnab/about-cnab.component';
 import { HeaderComponent } from '../header/header.component';
 import { OperationsListComponent } from '../operations-list/operations-list.component';
 import { FooterComponent } from '../footer/footer.component';
 import { BtnSelectComponent } from '../btn-select/btn-select.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    // CommonModule,
     HeaderComponent,
     OperationsListComponent,
     NgOptimizedImage,
     BtnSelectComponent,
     AboutCNABComponent,
-    FooterComponent
+    FooterComponent,
+    RouterLink
   ],
   templateUrl: './home.component.html',
 })
