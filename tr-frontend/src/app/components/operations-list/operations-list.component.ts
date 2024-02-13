837b9c35a4cf28887bb5fc8b009a85a931ff8a49
Jonas <jonascamargo999@gmail.com>
2024-02-13 00:11:55 -0300

    feat: add total value of transactions dynamically

--- tr-frontend/src/app/components/operations-list/operations-list.component.ts
+++ tr-frontend/src/app/components/operations-list/operations-list.component.ts
@@ -36,4 +36,15 @@ export class OperationsListComponent implements OnInit {
     this.operationService.removeOperations();
   }
 
+  formatValue(value: number): string {
+    return value.toLocaleString('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    });
+
+  }
+  
 }
+
