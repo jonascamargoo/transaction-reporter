dc64f59e15d68fd2854834077a2f009bcd6fbd1b
Jonas <jonascamargo999@gmail.com>
2024-02-15 12:29:51 -0300

    feat: add script to automatize docker init

---
+++ start.sh
@@ -0,0 +1,10 @@
+docker-compose down
+
+# build backend image
+docker build -t backend-transaction-reporter:latest ./tr-backend
+
+# build frontend image
+docker build -t frontend-transaction-reporter:latest ./tr-frontend
+
+# start environment - removendo caches e subindo os dois de uma vez
+docker-compose up --build --force-recreate --remove-orphans
