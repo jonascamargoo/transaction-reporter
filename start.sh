414b6b9d05b3f24bc73a3289980d8c8559b7e698
Jonas <jonascamargo999@gmail.com>
2024-05-27 18:07:39 -0300

    fix:  fix start script

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-docker compose down
+docker-compose down
 
 # build backend image
 docker build -t backend-transaction-reporter:latest ./tr-backend
@@ -7,4 +7,4 @@ docker build -t backend-transaction-reporter:latest ./tr-backend
 docker build -t frontend-transaction-reporter:latest ./tr-frontend
 
 # start environment - removendo caches e subindo os dois de uma vez
-docker compose up --build --force-recreate --remove-orphans
+docker-compose up --build --force-recreate --remove-orphans
